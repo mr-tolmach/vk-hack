d7aad53389659703d94c2b91dc481f489fb1bbe4
Kirik64rus <kzyemskov@mail.ru>
2017-10-21 13:42:10 +0300

    db

--- proxy/routes.js
+++ proxy/routes.js
@@ -2,12 +2,29 @@ const express = require('express'),
     router = express.Router(),
     https = require('https'),
     config = require('config'),
-    vkApi = require('./vk');
+    vkApi = require('./vk'),
+    db = require('./db');
 
-router.get('/card', (req, res) => {
+const getEvents = function (city) {
+    let data = db.getEventsForCity(city);
+    return data
+};
+
+
+router.get('/notification.send', (req, res) => {
     try {
-        vkApi.makeUserCard(req.query.id);
-        res.send(323)
+        vkApi.sendNotifications(req.query.id1, req.query.id2);
+        res.send({'result' : 'ok'})
+    } catch (err) {
+        console.log(`[FATAL ERROR] Get rating from db: error = ${err}`);
+        res.status(500).send({error: ""});
+    }
+});
+
+
+router.get('/events', (req, res) => {
+    try {
+        getEvents('spb').then(res => res.send({'result' : res}))
     } catch (err) {
         console.log(`[FATAL ERROR] Get rating from db: error = ${err}`);
         res.status(500).send({error: ""});
