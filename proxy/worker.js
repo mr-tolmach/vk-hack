5db4e032cffd6cfff3122f6ff019a329c04f43a9
Vladislav Kiryukhin <kiryukhinv@gmail.com>
2017-10-21 16:40:07 +0300

    headers

--- proxy/worker.js
+++ proxy/worker.js
@@ -8,6 +8,12 @@ const app = express(),
       config = require('config');
 
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'POST, GET, DELETE, PUT');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Cache-Control');
+    next();
+});
 app.use('/api', require('./routes'));
 
 
